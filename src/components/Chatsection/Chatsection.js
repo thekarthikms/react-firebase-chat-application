e465169e5d28322d7a046db207b8497a8160dfac
karthikms-msil <karthikms-msil@github.com>
2021-03-04 19:51:30 +0530

    real time message updates ...still bugs need to fix

--- src/components/Chatsection/Chatsection.js
+++ src/components/Chatsection/Chatsection.js
@@ -9,43 +9,48 @@ import { setMsg,clrMsg } from '../../redux/actions/messages_action';
 import {msgTog} from '../../redux/actions/messagelist_action'
 
 
+
 let Chatsection = (props)=>{
     let chat = props.chatselect.active
     useEffect(()=>{
-    //  if(props.chatselect.user.username){
-    //     fetchChat()
+     if(props.chatselect.user.username){
+        fetchChat()
         
-    //  }
+     }
         
     },[props.messagelist.messageListToggle])
 
-    // let fetchChat = ()=>{
+    let fetchChat = ()=>{
        
-    //     UserRef.doc(props.chatselect.user.userid).onSnapshot((snap)=>{
-    //         let messages = props.messages.messages
-    //         let CurrentUserList = snap.data().chats[props.userlog.user.username]
-    //         let i=0
-    //         CurrentUserList.forEach(async (chat)=>{
-    //             let ChatMessage  = await ChatRef.doc(`${chat}`).get()
-    //             let date = ChatMessage.data().timestamp.toDate()
-    //             let msg = [ChatMessage.data().message,snap.data().username,ChatMessage.id]
-              
-               
-    //         })
-            
-    //     })
-       
+        UserRef.where("username", "==",props.chatselect.user.username).onSnapshot((snapshot)=>{
+            snapshot.docChanges().forEach(async (change)=>{
+                if(change.type === "modified"){
+                    let arr = change.doc.data().chats[props.userlog.user.username]
+                    let chat = arr[arr.length-1]
+                    let ChatMessage = await ChatRef.doc(`${chat}`).get()
+                    let date = ChatMessage.data().timestamp.toDate()
+                    let messages = props.messages.messages
+                    props.clearmessage()
+                    messages.push([ChatMessage.data().message,date,props.chatselect.user.username,ChatMessage.id])
+                    props.setmessage(messages)
+                    props.messagetog()
+                }
+            })
+           
+        })
+        
+        // })
       
-    //     let ChatUserList = ChatUser.data().chats[CurrentUser.data().username]
-    //     ChatUserList.forEach(async (chat)=>{
-    //         let ChatMessage = await ChatRef.doc(`${chat}`).get()
-    //         let date = ChatMessage.data().timestamp.toDate()
-    //         msgList.push([ChatMessage.data().message,date,ChatUser.data().username])
-    //     })
-    //     props.chatSel(`${ChatUser.data().username}`,`${ChatUser.id}`)
-    //     props.setmessage(msgList)
+        // let ChatUserList = ChatUser.data().chats[CurrentUser.data().username]
+        // ChatUserList.forEach(async (chat)=>{
+        //     let ChatMessage = await ChatRef.doc(`${chat}`).get()
+        //     let date = ChatMessage.data().timestamp.toDate()
+        //     msgList.push([ChatMessage.data().message,date,ChatUser.data().username])
+        // })
+        // props.chatSel(`${ChatUser.data().username}`,`${ChatUser.id}`)
+        // props.setmessage(msgList)
        
-    // }
+    }
     return (
         <div className="chat-section-container">
             {chat
