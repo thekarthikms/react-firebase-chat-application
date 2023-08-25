6c8efaf894ec5273a151bf109c34509f6024b1e6
karthik ms <thekarthikms@github.com>
2023-08-25 22:31:04 +0530

    firebase web-config hidden

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,5 @@
 import firebase from 'firebase/firebase'
-const firebaseConfig = {
-  apiKey: "AIzaSyDdzVa72eFtGrjIb3j98q8cGMpjeGLMVIM",
-  authDomain: "firechat-7acda.firebaseapp.com",
-  projectId: "firechat-7acda",
-  storageBucket: "firechat-7acda.appspot.com",
-  messagingSenderId: "215612547241",
-  appId: "1:215612547241:web:559d89a728bde05cadc9b8"
-};
+import firebaseConfig from "./firebase-config"
 
 firebase.initializeApp(firebaseConfig)
 let db=firebase.firestore()
